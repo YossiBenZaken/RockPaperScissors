98b1deedebba9691f80ae30eb04143c028204461
YossiBenZaken <yossibz@gmail.com>
2022-02-24 11:00:55 +0200

    Challenge - Rock Paper Scissors

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var shouldWin: Bool = Bool.random()
+    @State private var score: Int = 0
+    @State private var turn: Int = 0
+    @State private var appMove: Int = Int.random(in: 0...2)
+    @State private var showingGameOver: Bool = false
+    let moves = ["✊", "✋", "✌️"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Your Score: \(score)")
+                .font(.title)
+            Spacer()
+            Text("I choose").font(.title)
+            Text(moves[appMove]).font(.system(size: 150))
+            Text("and you need to \(shouldWin ? "win" : "lose")").font(.title)
+            Spacer()
+            HStack {
+                ForEach(moves, id: \.self) { move in
+                    Button {
+                        moveSelect(move)
+                    } label: {
+                        Text(move).font(.system(size: 100))
+                    }
+                    
+                }
+            }
+            Spacer()
+            Spacer()
+        }
+        .alert("Game Over", isPresented: $showingGameOver) {
+            Button("Reset Game", action: reset)
+        }
+    }
+    func reset() {
+        appMove = Int.random(in: 0...2)
+        shouldWin = Bool.random()
+        turn = 0
+        score = 0
+    }
+    func moveSelect(_ emoji: String) {
+        if shouldWin {
+            let winningMoves = ["✋", "✌️", "✊"]
+            score += winningMoves[appMove] == emoji ? 1 : -1
+        } else {
+            let losingMoves = ["✌️","✊","✋"]
+            score += losingMoves[appMove] == emoji ? 1 : -1
+        }
+        turn += 1
+        appMove = Int.random(in: 0...2)
+        shouldWin.toggle()
+        if turn >= 10 {
+            showingGameOver = true
+        }
     }
 }
 
